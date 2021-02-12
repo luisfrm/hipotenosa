8191b42d809975a61069520b4811aeaded8f670c
Luis <Luisfrm_@outlook.com>
2021-02-12 14:16:18 -0400

    initial commit

---
+++ hipotenusa.php
@@ -0,0 +1,8 @@
+<?php
+$a=3;
+$b=4;
+$cSquared = $a *$a + $b * $b;
+$c= sqrt ($cSquared);
+echo "<h1> Calculo de la hipotenusa del triángulo rectángulo de lados 3cm y 4cm:</h1>";
+echo 'El valor de la hipotenusa del triángulo rectángulo es: '.$c. 'cm';
+?>
